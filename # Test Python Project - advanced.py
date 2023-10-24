d906cf311d43f543d02fe47d143c064d44b0b98f
emmcygn <148863794+emmcygn@users.noreply.github.com>
2023-10-24 14:27:25 +0000

    Most Complex - With User Interaction

---
+++ # Test Python Project - advanced.py
@@ -0,0 +1,10 @@
+# This is a simple line of code for python
+print("hello world")
+
+# here is an additional area to make things a bit more interesting
+
+# print("Hello, Manny!")
+
+myName = input("What is your Name? ")
+
+print("Hello: " + myName)
